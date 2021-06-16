6be0bd518f212c2310ab8cd7bc097084306c3af3
YCuatEo-we <zhuirengan129119@126.com>
2021-06-16 15:49:30 +0800

    20170418 增加学生成绩查询页面

--- helloVux/src/page/SearchStudentScore.vue
+++ helloVux/src/page/SearchStudentScore.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <search @result-click="resultClick" @on-change="getResult" :results="results" v-model="value" position="absolute" auto-scroll-to-top top="46px" @on-focus="onFocus" @on-cancel="onCancel"></search>
+    <search @result-click="resultClick" @on-change="getResult" :results="results" v-model="value" position="absolute" auto-scroll-to-top top="0px" @on-focus="onFocus" @on-cancel="onCancel"></search>
   </div>
 </template>
 
@@ -50,34 +50,6 @@
 </script>
 
 <style scoped>
-  .sub-item {
-    color: #888;
-  }
-  .center {
-    text-align: center;
-    padding-top: 20px;
-    color: #fff;
-    font-size: 18px;
-  }
-  .center img {
-    width: 120px;
-    height: 120px;
-    border-radius: 50%;
-    border: 4px solid #ececec;
-  }
-  .title_desc {
-    margin-top: 0;
-    color: #3a3a3a;
-    text-align: center;
-    font-size: 16px;
-    padding: 0;
-  }
-  .diffDate {
-    font-size: 30px;
-    color: red;
-  }
-  .blur_background {
-    background: url(../img/flower.jpg) no-repeat;
-    background-size:100%;
-  }
+
+
 </style>
